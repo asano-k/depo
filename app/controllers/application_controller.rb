821cb7896a9e70390f1e5d63af5df91c47efc4c6
Daigo Tokunaga <daigo@is-pro.net>
2015-11-13 03:30:24 +0000

    [WIP] add cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,4 +14,12 @@ class ApplicationController < ActionController::Base
       %w{store products}.include?(controller_name)
     end
 
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+
 end
